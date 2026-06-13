0510ef5efb16acb75066e369dc21f67166395627
hu-lisa <lisahu.kaboom@gmail.com>
2026-06-12 20:09:50 -0400

    local storage functionality

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,29 @@
+import { parseISO } from "date-fns";
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+    const [storedValue, setStoredValue] = useState<T>(() => {
+        try {
+            const item = localStorage.getItem(key);
+            if (item == null) {
+                return initialValue;
+            }
+            return JSON.parse(item, dateReviver);
+        } catch {
+            return initialValue;
+        }
+    }); //function version of usestate will only be called the first time the component renders
+
+    useEffect(() => { //when smth inside the dependency array changes, you want to run a side effect
+        localStorage.setItem(key, JSON.stringify(storedValue))
+    }, [storedValue, key]) //when storedValue changes, store it in local
+
+    return [storedValue, setStoredValue] as const;
+}
+
+function dateReviver(key: string, value: unknown) { //problem: we're returning a JSON string, so we need to revive it into a date
+    if (typeof value === "string" && /^\d{4}-\d{2}-\d{2}T/.test(value)) { //regex expr checks for an iso-formatted date, which is what stringified dates are
+        return parseISO(value);
+    }
+    return value;
+}
